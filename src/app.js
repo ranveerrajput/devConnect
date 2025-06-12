323fcbd1553863afb2977392d6aa2789988e10a5
ranveerrajput <ranveerrajput351@gmail.com>
2025-06-12 10:12:09 +0530

    added first user to database

--- src/app.js
+++ src/app.js
@@ -1,25 +1,39 @@
 const express = require("express");
-const connectDB = require("./config/database")
+const connectDB = require("./config/database");
 
 const app = express();
+const User = require("./models/user");
 
+app.post("/signup", async(req, res) => {
+  const userObj = {
+    firstName: "Sachin",
+    lastName: "Magar",
+    emailId: "sachin@gmail.com",
+    password: "12334",
+  };
+  const user = new User(userObj);
 
-app.use("/user",(req,res)=>{
-    console.log("from / route");
-    res.send("Hello ");
+  try {
+    await user.save();
+     res.send("User added Successfully!!!");
+  } catch (err) {
+    res.status(400).send("Falied to save the data: " + err.message);
+  }
 });
 
+app.use("/user", (req, res) => {
+  console.log("from / route");
+  res.send("Hello ");
+});
 
 
 connectDB()
   .then(() => {
     console.log("DB Connected Successfully....");
-    app.listen(7777,()=>{
-    console.log("Server is running successfully on port 7777");
-});
+    app.listen(7777, () => {
+      console.log("Server is running successfully on port 7777");
+    });
   })
   .catch((err) => {
     console.log("DB connection Failed!!!");
   });
-
-
